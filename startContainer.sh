4d862aef9e2d2f852db0d914d902a3e78dc70a39
Michalis Psalios <psalios10@gmail.com>
2018-02-12 22:29:16 +0000

    delete files from root

--- startContainer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-copyVersionFile() {
-    cat /python-server.version > /version-store/python-server.version
-}
-
-copyVersionFile
-cd /usr/local/python-server
-python Spectrum.py
--- tools/fixedWheelZoomTool.py
+++
@@ -1,8 +0,0 @@
-from bokeh.models import Scroll
-from bokeh.core.properties import List, Enum
-from bokeh.core.enums import Dimensions
-
-class FixedWheelZoomTool(Scroll):
-    __implementation__ = "fixedWheelZoomTool.ts"
-
-    dimensions = Enum(Dimensions, default="both")
